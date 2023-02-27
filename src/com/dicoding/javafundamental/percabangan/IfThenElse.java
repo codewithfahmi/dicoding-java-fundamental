1afdaacfcc6eb1bdce91a78d1ba80f85c0a29366
codewithfahmi <fahmiyahya0@gmail.com>
2023-02-27 23:35:38 +0700

    Menambahkan package percabangan

---
+++ src/com/dicoding/javafundamental/percabangan/IfThenElse.java
@@ -0,0 +1,13 @@
+package com.dicoding.javafundamental.percabangan;
+
+public class IfThenElse {
+    public static void main(String[] args) {
+        boolean isOn = true;
+
+        if (isOn){
+            System.out.println("Menyalakan Lampu");
+        }else{
+            System.out.println("Kondisi tidak terpenuhi...");
+        }
+    }
+}
